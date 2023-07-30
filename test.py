1bcdeae93bd22366c1d6188d8bcefc4f40350c38
Wiam_Laarabi <wiamlaarabi2001@gmail.com>
2023-07-30 22:56:56 +0100

    modify test.py

--- test.py
+++ test.py
@@ -5,7 +5,12 @@ options = webdriver.ChromeOptions()
 options.add_argument("--headless")  # Run Chrome in headless mode
 options.add_argument("--no-sandbox")  # Required when running as root user
 driver = webdriver.Chrome(options=options)
-driver.get("http://127.0.0.1:8000/")
-print(driver.title)
-time.sleep(10)  # Reduced the sleep time for demonstration purposes
+
+try:
+    driver.get("http://127.0.0.1:8000/")
+    print("Page title:", driver.title)
+except Exception as e:
+    print("Error accessing the page:", e)
+
+# Add this line to make sure the WebDriver is properly closed
 driver.quit()
